c973503fae11adac26ecfc7337191bda3db26ecf
aap <_aap_@mail.ru>
2020-05-22 11:39:01 +0300

    ломалка coreAgenta

--- CoreAgent/main.cpp
+++ CoreAgent/main.cpp
@@ -1,9 +1,28 @@
 #include <Global.h>
 #include <QAndroidService>
-
+#include <QThread>
 int main(int argc, char *argv[])
 {
-  QAndroidService app(argc, argv);
-  DF("++++++++ QAndroidService CoreAgent ++++++++");
-  return app.exec();
+  /*CoreAgent*/ QAndroidService app(argc, argv);
+  static QString TAG = "qt_LauncherActivity";
+  QThread thread;
+  int zz = 0;
+  qInfo() << "QQuickViewer setup completed  qt -----------------------------++++++++";
+
+  QObject::connect(&thread, &QThread::started, [&]() {
+    qDebug() << "qt waiting... -----------------------------------";
+    QThread::msleep(5000);
+    qDebug() << "qt done---------------------------------++++++++";
+    int aa = 10 / zz;
+    app.exit(aa);
+    qDebug() << "qt app.quit---------------------------------++++++++";
+  });
+  thread.start();
+
+  qDebug() << "qt ++++++++ CoreAgent ++++++++++++++++";
+  int result = app.exec();
+
+  thread.quit();
+  result += thread.wait();
+  return result;
 }
